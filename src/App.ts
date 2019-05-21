0bfc948c17e662a5a24f4eab0e40a75ad7f4a078
frickreich <frickreich@gmail.com>
2019-05-21 10:15:45 +0200

    Rollback build.

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,5 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 import { Main } from './Main';
 
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom'
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import { App } from './App';
 
 import { Home } from './components/Home.react';
-import { List } from './components/List.react';
-import { Test } from './components/Test.react';
-import { NotFound } from './components/NotFound';
 
 import './style.css';
 
@@ -23,35 +19,11 @@ export class Main extends React.Component<IMainProps, {}>
         super(props);
     }
 
-    routing = (
-        <Router >
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/list">List</Link>
-                    </li>
-                    <li>
-                        <Link to="/Test">Test</Link>
-                    </li>
-                </ul>
-                <Switch>
-                    <Route exact path="/" component={ Home } />
-                    <Route path="/list/:id" component={ List} />
-                    <Route path="/Test" component={ Test } />
-                    <Route component={ NotFound } />
-                </Switch>
-            </div>
-        </Router>
-    );
-
     public render(): JSX.Element
     {
         return (
             <>
-                { this.routing }
+                <Home />
             </>
         );
     }
